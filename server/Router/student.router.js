315f782d0da75b69b47b528b847444e7dcb3485c
rohit <rohitmathur607@gmail.com>
2023-04-19 16:44:34 +0530

    first website

---
+++ server/Router/student.router.js
@@ -0,0 +1,22 @@
+
+const studentRouter = (method,res) => {
+    switch(method) {
+        case "POST" :
+              
+            return;
+        case "GET" :
+            return;
+        case "PUT" :
+            return;
+        case "PATCH":
+            return;
+        case "DELETE" :
+            return
+        default : 
+            res.whiteHead(405,{ "contect-type" : "application/json"})
+            res.end(JSON.stringify({ msg : "Method not allowed"}));
+        return;
+    }
+}
+
+module.exports = studentRouter
